6b53903f6f35d7cb8072b4911e5572f2d8db539b
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2021-01-18 21:44:36 +0530

    created React app, added Paletts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Palette {...seedColors[5]} />
+      </div>
+    );
+  }
 }
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navigation bar goes here */}
+        <div className="Palette-colors"></div>
+        {/*Footer goes here*/}
+      </div>
+    );
+  }
+}
+
+export default Palette;
