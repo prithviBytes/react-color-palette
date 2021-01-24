66d3fc2d7c7364181b37a54c56d73ed0203ef0f7
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2021-01-24 21:20:41 +0530

    Connect colorpicker to button

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -73,9 +73,16 @@ const styles = (theme) => ({
 });
 
 class NewPaletteForm extends Component {
-  state = {
-    open: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: true,
+      currentColor: "teal",
+      colors: ["purple", "green"]
+    };
+    this.updateCurrentColor = this.updateCurrentColor.bind(this);
+    this.addNewColor = this.addNewColor.bind(this);
+  }
 
   handleDrawerOpen = () => {
     this.setState({ open: true });
@@ -84,6 +91,16 @@ class NewPaletteForm extends Component {
   handleDrawerClose = () => {
     this.setState({ open: false });
   };
+  updateCurrentColor(newColor) {
+    this.setState({
+      currentColor: newColor.hex
+    });
+  }
+  addNewColor() {
+    this.setState({
+      colors: [...this.state.colors, this.state.currentColor]
+    });
+  }
   render() {
     const { classes } = this.props;
     const { open } = this.state;
@@ -134,8 +151,16 @@ class NewPaletteForm extends Component {
               Random Color
             </Button>
           </div>
-          <ChromePicker color="purple" />
-          <Button variant="contained" color="primary">
+          <ChromePicker
+            color={this.state.currentColor}
+            onChangeComplete={this.updateCurrentColor}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ backgroundColor: this.state.currentColor }}
+            onClick={this.addNewColor}
+          >
             Add Color
           </Button>
         </Drawer>
@@ -145,6 +170,11 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
+          <ul>
+            {this.state.colors.map((color) => (
+              <li>{color}</li>
+            ))}
+          </ul>
         </main>
       </div>
     );
