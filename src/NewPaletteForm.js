4e156d94b2c2451e6b10ed031899c29744a7b320
Prithvi Suvarna <prithvisuvarna56@gmail.com>
2021-01-31 17:56:08 +0530

    Add Random Color Variable

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -77,10 +77,12 @@ class NewPaletteForm extends Component {
   }
   addRandomColor() {
     const allColors = this.props.palettes.map((p) => p.colors).flat();
+    let rand;
+    let randomColor;
     let isDuplicateColor = true;
     while (isDuplicateColor) {
-      let rand = Math.floor(Math.random() * allColors.length);
-      let randomColor = allColors[rand];
+      rand = Math.floor(Math.random() * allColors.length);
+      randomColor = allColors[rand];
       isDuplicateColor = this.state.colors.some(
         (color) => color.name === randomColor.name
       );
